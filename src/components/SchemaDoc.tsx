e5aa2305d4a417da09ff1eea43e1eadbb07f3108
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2021-12-10 13:05:41 +0100

    fix: prevent unnecessary editor updates

---
+++ src/components/SchemaDoc.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
+import {
+  SchemaExplorer,
+  SchemaExplorerProps,
+  Lookup,
+  InternalLookup,
+  PathElement,
+  getSchemaFromReference,
+  JsonSchema,
+} from "@kickstartds/json-schema-viewer";
+
+const getTitle = (schema: JsonSchema | undefined): string => {
+  if (schema === undefined) {
+    return "<not found>";
+  }
+
+  if (typeof schema === "boolean") {
+    return "<anything>";
+  }
+
+  return schema.title || "object";
+};
+
+const removeLeadingSlash = (v: string): string => {
+  if (v.startsWith("/")) {
+    return v.slice(1);
+  }
+  return v;
+};
+
+type SchemaDocProps = {
+  schema: JsonSchema;
+};
+
+export const SchemaDoc: React.FC<SchemaDocProps> = ({ schema }) => {
+  const lookup = new InternalLookup(schema);
+
+  const getPathFromRoute = (lookup: Lookup): Array<PathElement> => {
+    const basePathSegments = ["base"];
+    const { pathname } = useLocation();
+    const pathSegments = removeLeadingSlash(pathname).split("/");
+    let iterator = 0;
+    while (
+      pathSegments[iterator] !== undefined &&
+      basePathSegments[iterator] !== undefined &&
+      basePathSegments[iterator] === pathSegments[iterator]
+    ) {
+      iterator++;
+    }
+
+    if (iterator === pathSegments.length) {
+      const reference = "#";
+      const title = getTitle(getSchemaFromReference(reference, lookup));
+      return [
+        {
+          title,
+          reference,
+        },
+      ];
+    }
+
+    return pathSegments
+      .slice(iterator)
+      .map(decodeURIComponent)
+      .map((userProvidedReference) => {
+        const reference = userProvidedReference.startsWith("#")
+          ? userProvidedReference
+          : "#/invalid-reference";
+        const title = getTitle(getSchemaFromReference(reference, lookup));
+        return {
+          title,
+          reference,
+        };
+      });
+  };
+
+  const path = getPathFromRoute(lookup);
+  if (path.length === 0) {
+    return <div>Error: Could not work out what to load from the schema.</div>;
+  }
+
+  const currentPathElement = path[path.length - 1];
+  const currentSchema = getSchemaFromReference(
+    currentPathElement.reference,
+    lookup
+  );
+
+  if (currentSchema === undefined) {
+    return (
+      <div>
+        ERROR: Could not look up the schema that was requested in the URL.
+      </div>
+    );
+  }
+
+  if (typeof currentSchema === "boolean") {
+    return (
+      <div>TODO: Implement anything or nothing schema once clicked on.</div>
+    );
+  }
+
+  const explorerArgs: SchemaExplorerProps = {
+    basePathSegments: ["base"],
+    path,
+    lookup,
+    schema: currentSchema,
+    stage: "both",
+  };
+
+  return <SchemaExplorer {...explorerArgs} />;
+};
--- src/components/SchemaEditor.tsx
+++ src/components/SchemaEditor.tsx
@@ -1,14 +1,20 @@
-import React, { useEffect, useRef } from "react";
-import Editor, { useMonaco, OnMount, OnValidate, OnChange } from "@monaco-editor/react";
+import React, { useEffect, useRef, useMemo } from "react";
+import Editor, {
+  useMonaco,
+  OnMount,
+  OnValidate,
+  OnChange,
+} from "@monaco-editor/react";
 import { JsonSchema } from "@kickstartds/json-schema-viewer";
+import { pack, unpack } from "@kickstartds/core/lib/storybook/helpers";
+import { useArgs } from "@storybook/api";
+import decomment from "decomment";
 
 type OnChangeParams = Parameters<OnChange>;
 
 type SchemaEditorProps = {
-  initialContent: unknown;
   schema: JsonSchema;
-  handleValidChange: (value: OnChangeParams[0]) => void;
-}
+};
 
 const editorPreamble = `
 // Copy-and-paste your JSON in here to live-edit
@@ -16,17 +22,26 @@ const editorPreamble = `
 // benefit of validation and autocompletion!
 `.trim();
 
-export const SchemaEditor: React.FC<SchemaEditorProps> = (props) => {
+export const SchemaEditor: React.FC<SchemaEditorProps> = ({ schema }) => {
   const editorRef = useRef(null);
   const monaco = useMonaco();
+  const [storybookArgs, updateArgs] = useArgs();
+
+  const initialContent = useMemo(() => unpack(storybookArgs), [schema]);
 
   const handleEditorDidMount: OnMount = (editor, monaco) => {
     editorRef.current = editor;
   };
 
+  const handleChange = (value: OnChangeParams[0]) => {
+    try {
+      updateArgs(pack(JSON.parse(decomment(value))));
+    } catch (e) {}
+  };
+
   const handleEditorValidChange: OnValidate = (markers) => {
     if (markers.length === 0 && editorRef && editorRef.current) {
-      props.handleValidChange(editorRef.current.getValue());
+      handleChange(editorRef.current.getValue());
     }
   };
 
@@ -34,27 +49,27 @@ export const SchemaEditor: React.FC<SchemaEditorProps> = (props) => {
     monaco?.languages.json.jsonDefaults.setDiagnosticsOptions({
       validate: true,
       allowComments: true,
-      schemas: [{
-        uri: "https://json-schema.app/example.json", // id of the first schema
-        fileMatch: ['a://b/example.json'],
-        schema: props.schema
-      }]
+      schemas: [
+        {
+          uri: "https://json-schema.app/example.json", // id of the first schema
+          fileMatch: ["a://b/example.json"],
+          schema: schema,
+        },
+      ],
     });
-  }, [monaco, props.schema]);
+  }, [monaco, schema]);
 
   return (
     <Editor
       height="97vh"
       defaultLanguage="json"
-      value={
-        editorPreamble + "\n" + JSON.stringify(props.initialContent, null, 2)
-      }
+      value={editorPreamble + "\n" + JSON.stringify(initialContent, null, 2)}
       path="a://b/example.json"
       theme="vs-dark"
       onValidate={handleEditorValidChange}
       saveViewState={false}
       onMount={handleEditorDidMount}
-      onChange={props.handleValidChange}
+      onChange={handleChange}
     />
   );
 };
